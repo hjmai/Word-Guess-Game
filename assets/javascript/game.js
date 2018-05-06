677d3b1558513080eaf409d427bca7bc35742a13
Jonathan Mai <jmai@sold.com>
2018-05-05 23:44:39 -0700

    Fixed formatting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var wordArr = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise","Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill","Pidgey","Pidgeotto","Pidgeot","Rattata","Raticate","Spearow","Fearow","Ekans","Arbok","Pikachu","Raichu","Sandshrew","Sandslash","Nidoran","Nidorina","Nidoqueen","Nidoran","Nidorino","Nidoking","Clefairy","Clefable","Vulpix","Ninetales","Jigglypuff","Wigglytuff","Zubat","Golbat","Oddish","Gloom","Vileplume","Paras","Parasect","Venonat","Venomoth","Diglett","Dugtrio","Meowth","Persian","Psyduck","Golduck","Mankey","Primeape","Growlithe","Arcanine","Poliwag","Poliwhirl","Poliwrath","Abra","Kadabra","Alakazam","Machop","Machoke","Machamp","Bellsprout","Weepinbell","Victreebel","Tentacool","Tentacruel","Geodude","Graveler","Golem","Ponyta","Rapidash","Slowpoke","Slowbro","Magnemite","Magneton","Farfetch'd","Doduo","Dodrio","Seel","Dewgong","Grimer","Muk","Shellder","Cloyster","Gastly","Haunter","Gengar","Onix","Drowzee","Hypno","Krabby","Kingler","Voltorb","Electrode","Exeggcute","Exeggutor","Cubone","Marowak","Hitmonlee","Hitmonchan","Lickitung","Koffing","Weezing","Rhyhorn","Rhydon","Chansey","Tangela","Kangaskhan","Horsea","Seadra","Goldeen","Seaking","Staryu","Starmie","Mr. Mime","Scyther","Jynx","Electabuzz","Magmar","Pinsir","Tauros","Magikarp","Gyarados","Lapras","Ditto","Eevee","Vaporeon","Jolteon","Flareon","Porygon","Omanyte","Omastar","Kabuto","Kabutops","Aerodactyl","Snorlax","Articuno","Zapdos","Moltres","Dratini","Dragonair","Dragonite","Mewtwo","Mew"];
+var wordArr = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Rattata", "Raticate", "Spearow", "Fearow", "Ekans", "Arbok", "Pikachu", "Raichu", "Sandshrew", "Sandslash", "Nidoran", "Nidorina", "Nidoqueen", "Nidoran", "Nidorino", "Nidoking", "Clefairy", "Clefable", "Vulpix", "Ninetales", "Jigglypuff", "Wigglytuff", "Zubat", "Golbat", "Oddish", "Gloom", "Vileplume", "Paras", "Parasect", "Venonat", "Venomoth", "Diglett", "Dugtrio", "Meowth", "Persian", "Psyduck", "Golduck", "Mankey", "Primeape", "Growlithe", "Arcanine", "Poliwag", "Poliwhirl", "Poliwrath", "Abra", "Kadabra", "Alakazam", "Machop", "Machoke", "Machamp", "Bellsprout", "Weepinbell", "Victreebel", "Tentacool", "Tentacruel", "Geodude", "Graveler", "Golem", "Ponyta", "Rapidash", "Slowpoke", "Slowbro", "Magnemite", "Magneton", "Farfetch'd", "Doduo", "Dodrio", "Seel", "Dewgong", "Grimer", "Muk", "Shellder", "Cloyster", "Gastly", "Haunter", "Gengar", "Onix", "Drowzee", "Hypno", "Krabby", "Kingler", "Voltorb", "Electrode", "Exeggcute", "Exeggutor", "Cubone", "Marowak", "Hitmonlee", "Hitmonchan", "Lickitung", "Koffing", "Weezing", "Rhyhorn", "Rhydon", "Chansey", "Tangela", "Kangaskhan", "Horsea", "Seadra", "Goldeen", "Seaking", "Staryu", "Starmie", "Mr. Mime", "Scyther", "Jynx", "Electabuzz", "Magmar", "Pinsir", "Tauros", "Magikarp", "Gyarados", "Lapras", "Ditto", "Eevee", "Vaporeon", "Jolteon", "Flareon", "Porygon", "Omanyte", "Omastar", "Kabuto", "Kabutops", "Aerodactyl", "Snorlax", "Articuno", "Zapdos", "Moltres", "Dratini", "Dragonair", "Dragonite", "Mewtwo", "Mew"];
 var wordPicked = "";
 var letterArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var correctLetters = [];
@@ -11,49 +11,48 @@ var attempts = 10;
 var winCount = 0;
 
 /* All Functions Go Here*/
-function pickWord(){
+function pickWord() {
     reset();
-    wordPicked = wordArr[Math.floor(Math.random()*wordArr.length)].toLowerCase();
+    wordPicked = wordArr[Math.floor(Math.random() * wordArr.length)].toLowerCase();
     attempts = 10;
     correctLetters = [];
     wrongLetters = [];
 }
 
-function reset(){
+function reset() {
     guessWord.innerHTML = "";
     wrongLetterSpan.innerHTML = "";
 }
 
-function skip(){
+function skip() {
     pickWord();
     populateText();
 }
 
-function populateText(){
+function populateText() {
     reset();
     attemptSpan.innerHTML = "";
     winSpan.innerHTML = "";
 
     // Display remaining letters
-    for(var i = 0; i < wordPicked.length; i++){
+    for (var i = 0; i < wordPicked.length; i++) {
         var char = document.createElement(null);
-        if(correctLetters.indexOf(wordPicked[i].toLowerCase()) >= 0){
-            
+        if (correctLetters.indexOf(wordPicked[i].toLowerCase()) >= 0) {
             char.textContent = wordPicked[i];
         }
-        else{
+        else {
             char.textContent = " _ ";
         }
         guessWord.appendChild(char);
     }
 
     // Display Wrong Letters
-    for(var k = 0; k < wrongLetters.length; k++){
+    for (var k = 0; k < wrongLetters.length; k++) {
         var char = document.createElement(null);
-        char.textContent = wrongLetters[k]+' ';
+        char.textContent = wrongLetters[k] + ' ';
         wrongLetterSpan.appendChild(char);
     }
-    
+
     // Display Remaining Attempts
     var attemptLeft = document.createElement(null);
     attemptLeft.textContent = attempts;
@@ -65,25 +64,25 @@ function populateText(){
     winSpan.appendChild(wins);
 }
 
-document.onkeyup = function(event){
-    if(letterArray.indexOf(event.key) >= 0){
-        if(wrongLetters.indexOf(event.key) === -1 && event.key !== 'Enter'){
-            if(wordPicked.indexOf(event.key) >= 0){
+document.onkeyup = function (event) {
+    if (letterArray.indexOf(event.key) >= 0) {
+        if (wrongLetters.indexOf(event.key) === -1 && event.key !== 'Enter') {
+            if (wordPicked.indexOf(event.key) >= 0) {
                 correctLetters.push(event.key);
             }
-            else{
+            else {
                 wrongLetters.push(event.key);
                 attempts--;
-                if(attempts == 0){
+                if (attempts == 0) {
                     pickWord();
                 }
             }
         }
     }
-    if(event.key === 'Enter'){
-        if(wordPicked === guessWord.textContent){
+    if (event.key === 'Enter') {
+        if (wordPicked === guessWord.textContent) {
             winCount++;
-            reset();         
+            reset();
             pickWord();
         }
     }
